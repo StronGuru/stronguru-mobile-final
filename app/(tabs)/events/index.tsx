3a532b0206cf016d7c118e83f5decf5b56229eec
notdav04 <davide.alonzi26@gmail.com>
2025-09-15 10:42:05 +0200

    add events config

--- app/(tabs)/events/index.tsx
+++ app/(tabs)/events/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-export default function Events() {
+export default function EventsScreen() {
   return (
     <View>
       <Text>Events</Text>
